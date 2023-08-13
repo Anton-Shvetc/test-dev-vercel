adf7e9e55ccaf8a9cfc0ccb79ec3cf6426667abd
Anton <antonshvetswork@yandex.ru>
2023-08-13 21:49:11 +0300

    test

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 app.use("/login", (req, res) => {
   res.send("Hello World!");
 });
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 app.listen(port, () =>
   console.log(`Server running on ${port}, http://localhost:${port}`)
